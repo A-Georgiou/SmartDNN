1b016c65ad362b12439ff1d6e3a147e8958cff2a
Andrew Georgiou <andrewgeorgiou98@outlook.com>
2024-08-18 00:31:27 +0100

    Clean up CMakeLists for testing
    * Add automatically glob recovery for *.cpp in smart_dnn directory
    
    * Separate main function to main.cpp file in test dir
    
    * Added debugging statements for Test / Main source globs

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
+
+    std::cout << "RUNNING" << std::endl;
+
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
